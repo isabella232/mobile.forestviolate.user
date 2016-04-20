37082fe0a9e643063b272e3493e4524d6c2c958a
Stanislav Petryakov <become.glory@yandex.ru>
2016-04-20 15:16:11 +0300

    Change WWF about info, fix #58

--- app/src/main/java/com/nextgis/safeforest/activity/AboutActivity.java
+++ app/src/main/java/com/nextgis/safeforest/activity/AboutActivity.java
@@ -22,20 +22,23 @@
 
 package com.nextgis.safeforest.activity;
 
-import android.content.pm.PackageManager;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.nextgis.maplibui.activity.NGActivity;
+import com.nextgis.safeforest.BuildConfig;
 import com.nextgis.safeforest.R;
 
 
-public class AboutActivity extends NGActivity
+public class AboutActivity extends NGActivity implements View.OnClickListener
 {
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -46,33 +49,34 @@ public class AboutActivity extends NGActivity
         setToolbar(R.id.main_toolbar);
 
         TextView txtVersion = (TextView) findViewById(R.id.app_version);
-        try {
-            String pkgName = this.getPackageName();
-            PackageManager pm = this.getPackageManager();
-            String versionName = pm.getPackageInfo(pkgName, 0).versionName;
-            String versionCode =
-                    Integer.toString(pm.getPackageInfo(this.getPackageName(), 0).versionCode);
-            txtVersion.setText("v. " + versionName + " (rev. " + versionCode + ")");
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-            txtVersion.setText("");
-        }
+        txtVersion.setText(String.format(getString(R.string.version), BuildConfig.VERSION_NAME, BuildConfig.VERSION_CODE));
+
+        ImageView logo = (ImageView) findViewById(R.id.logo);
+        logo.setOnClickListener(this);
 
         Button credits = (Button) findViewById(R.id.credits);
-        credits.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                AlertDialog builder = new AlertDialog.Builder(view.getContext(), R.style.AppCompatDialog).setTitle(R.string.credits_intro)
+        credits.setOnClickListener(this);
+
+        TextView txtCopyrightText = (TextView) findViewById(R.id.copyright);
+        txtCopyrightText.setText(Html.fromHtml(getString(R.string.copyright)));
+        txtCopyrightText.setMovementMethod(LinkMovementMethod.getInstance());
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.credits:
+                AlertDialog builder = new AlertDialog.Builder(this, R.style.AppCompatDialog).setTitle(R.string.credits_intro)
                         .setMessage(R.string.credits)
                         .setPositiveButton(android.R.string.ok, null).create();
                 builder.show();
                 ((TextView) builder.findViewById(android.R.id.message)).setMovementMethod(LinkMovementMethod.getInstance());
                 ((TextView) builder.findViewById(android.R.id.message)).setLinksClickable(true);
-            }
-        });
-
-        TextView txtCopyrightText = (TextView) findViewById(R.id.copyright);
-        txtCopyrightText.setText(Html.fromHtml(getString(R.string.copyright)));
-        txtCopyrightText.setMovementMethod(LinkMovementMethod.getInstance());
+                break;
+            case R.id.logo:
+                Intent url = new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.wwf_link)));
+                startActivity(url);
+                break;
+        }
     }
 }
